da7dcf76feae68dc391bb86f303d37559fc9bc7e
Nursultan15 <73534336+Nursultan15@users.noreply.github.com>
2021-03-21 21:20:49 +0600

    Update GameField.java

--- GameField.java
+++ GameField.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,22 +7,22 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener{
-    private final int SIZE = 320;
-    private final int DOT_SIZE = 16;
-    private final int ALL_DOTS = 400;
-    private Image dot;
+    private final int A = 320;
+    private final int B = 16;
+    private final int C = 400;
+    private Image D;
     private Image apple;
-    private int appleX;
-    private int appleY;
-    private int[] x = new int[ALL_DOTS];
-    private int[] y = new int[ALL_DOTS];
-    private int dots;
-    private Timer timer;
+    private int X;
+    private int Y;
+    private int[] x = new int[C];
+    private int[] y = new int[C];
+    private int M;
+    private Timer T;
     private boolean left = false;
     private boolean right = true;
     private boolean up = false;
     private boolean down = false;
-    private boolean inGame = true;
+    private boolean IN = true;
 
 
     public GameField(){
@@ -36,93 +35,93 @@ public class GameField extends JPanel implements ActionListener{
     }
 
     public void initGame(){
-        dots = 3;
-        for (int i = 0; i < dots; i++) {
-            x[i] = 48 - i*DOT_SIZE;
+        M = 3;
+        for (int i = 0; i < M; i++) {
+            x[i] = 48 - i*B;
             y[i] = 48;
         }
-        timer = new Timer(250,this);
-        timer.start();
+        T = new Timer(250,this);
+        T.start();
         createApple();
     }
 
     public void createApple(){
-        appleX = new Random().nextInt(20)*DOT_SIZE;
-        appleY = new Random().nextInt(20)*DOT_SIZE;
+        X = new Random().nextInt(20)*B;
+        Y = new Random().nextInt(20)*B;
     }
 
     public void loadImages(){
         ImageIcon iia = new ImageIcon("apple11.png");
         apple = iia.getImage();
         ImageIcon iid = new ImageIcon("dot_icon_2.png");
-        dot = iid.getImage();
+        D = iid.getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if(inGame){
-            g.drawImage(apple,appleX,appleY,this);
-            for (int i = 0; i < dots; i++) {
-                g.drawImage(dot,x[i],y[i],this);
+        if(IN){
+            g.drawImage(apple,X,Y,this);
+            for (int i = 0; i < M; i++) {
+                g.drawImage(D,x[i],y[i],this);
             }
         } else{
             String str = "Game Over";
             //Font f = new Font("Arial",14,Font.BOLD);
             g.setColor(Color.white);
             // g.setFont(f);
-            g.drawString(str,125,SIZE/2);
+            g.drawString(str,125,A/2);
         }
     }
 
     public void move(){
-        for (int i = dots; i > 0; i--) {
+        for (int i = M; i > 0; i--) {
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
         if(left){
-            x[0] -= DOT_SIZE;
+            x[0] -= B;
         }
         if(right){
-            x[0] += DOT_SIZE;
+            x[0] += B;
         } if(up){
-            y[0] -= DOT_SIZE;
+            y[0] -= B;
         } if(down){
-            y[0] += DOT_SIZE;
+            y[0] += B;
         }
     }
 
     public void checkApple(){
-        if(x[0] == appleX && y[0] == appleY){
-            dots++;
+        if(x[0] == X && y[0] == Y){
+            M++;
             createApple();
         }
     }
 
     public void checkCollisions(){
-        for (int i = dots; i >0 ; i--) {
+        for (int i = M; i >0 ; i--) {
             if(i>4 && x[0] == x[i] && y[0] == y[i]){
-                inGame = false;
+                IN = false;
             }
         }
 
-        if(x[0]>SIZE){
-            inGame = false;
+        if(x[0]>A){
+            IN = false;
         }
         if(x[0]<0){
-            inGame = false;
+            IN = false;
         }
-        if(y[0]>SIZE){
-            inGame = false;
+        if(y[0]>A){
+            IN = false;
         }
         if(y[0]<0){
-            inGame = false;
+            IN = false;
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(inGame){
+        if(IN){
             checkApple();
             checkCollisions();
             move();
